c5aee41dddd370c59c332599ff26f01883758f2b
lewistjones <lewis@lewistjones.com>
2018-11-06 06:28:26 +0000

    Started section 11 and renamed all section folders so that they show up in order in  the browser

---
+++ section10-JavaScriptBasics/JSStalker.js
@@ -0,0 +1,5 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your second name?");
+var age = prompt("What is your age?");
+
+console.log(firstName + " " + lastName + " is " + age + " years old.")
---
+++ section10-JavaScriptBasics/ageCalc.js
@@ -0,0 +1,4 @@
+age = prompt("How old are you?");
+days = age * 365.25;
+
+alert(age + " is roughly " + days + " days.")
